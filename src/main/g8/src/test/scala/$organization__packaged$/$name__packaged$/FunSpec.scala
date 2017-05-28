0914e2dd2fd3faa9d7ba1904d008c73f9cc0edce
MrPowers <matthewkevinpowers@gmail.com>
2017-05-27 20:52:53 -0500

    Add the test directory structure

---
+++ src/main/g8/src/test/scala/$organization__packaged$/$name__packaged$/FunSpec.scala
@@ -0,0 +1,15 @@
+package $organization$.$name$
+
+import org.scalatest.FunSpec
+
+class FunSpec extends FunSpec {
+
+  describe(".beNice") {
+
+    it("tells you how to treat your mother") {
+      assert(Fun.beNice() === "Be nice to your mother!")
+    }
+
+  }
+
+}
