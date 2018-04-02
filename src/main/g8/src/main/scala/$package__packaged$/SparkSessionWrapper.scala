8c2c79841209103225fe0ee7c2c65951e5fda0cd
MrPowers <matthewkevinpowers@gmail.com>
2018-04-01 23:03:53 -0400

    Add the SparkSessionWrapper

---
+++ src/main/g8/src/main/scala/$package__packaged$/SparkSessionWrapper.scala
@@ -0,0 +1,11 @@
+package $package$
+
+import org.apache.spark.sql.SparkSession
+
+trait SparkSessionWrapper extends Serializable {
+
+  lazy val spark: SparkSession = {
+    SparkSession.builder().master("local").appName("spark session").getOrCreate()
+  }
+
+}
