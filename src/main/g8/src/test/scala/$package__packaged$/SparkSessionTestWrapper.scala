392a3ce4ca33e21b9193d5315e82fd813854c132
MrPowers <matthewkevinpowers@gmail.com>
2020-11-24 08:43:31 -0500

    Turn the annoying info logging when the test suite is run off

--- src/main/g8/src/test/scala/$package__packaged$/SparkSessionTestWrapper.scala
+++ src/main/g8/src/test/scala/$package__packaged$/SparkSessionTestWrapper.scala
@@ -1,10 +1,12 @@
 package $package$
 
 import org.apache.spark.sql.SparkSession
+import org.apache.log4j.{Logger, Level}
 
 trait SparkSessionTestWrapper {
 
   lazy val spark: SparkSession = {
+    Logger.getLogger("org").setLevel(Level.OFF)
     SparkSession.builder().master("local").appName("spark session").getOrCreate()
   }
 
